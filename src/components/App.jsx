6b3c45453b211cd4fed282a78cb2c2ced64f7b95
Kyle Treman <jkt290@nyu.edu>
2018-05-14 12:37:11 -0400

    Fix fetch to work with redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,32 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { setCategories } from '../actions'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { setCategories } from "../actions";
 
 class App extends Component {
 
-  async componentDidMount() {
-    if (this.props.categories.length === 0) {
-      const response = await fetch("http://jservice.io/api/categories?count=20");
-      const json = await response.json();
-      this.props.setCategories(json);
-    }
+  componentWillMount() {
+     this.getCategories();
+  }
+
+  getCategories = async () => {
+    const response = await fetch("http://jservice.io/api/categories?count=20");
+    const categories = await response.json();
+    this.props.setCategories(categories);
   }
 
   render() {
-    console.log(this.props)
+    console.log(this.props);
 
     return (
       <div>
         <h2>Jeopardy!</h2>
       </div>
-    )
+    );
   }
 }
 // setting redux state
-const mapStateToProps = (state) => {
-  return { categories: state }
-}
+const mapStateToProps = state => {
+  return { categories: state };
+};
 // connect(mapStateToProps, { what needs to be available as props })(Component)
 export default connect(mapStateToProps, { setCategories })(App);
