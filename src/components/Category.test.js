0cfa34cbaa7d7a68a11d9cf3075d1fec2b6d253e
Kyle Treman <jkt290@nyu.edu>
2018-05-14 14:34:54 -0400

    Add mock server

--- src/components/Category.test.js
+++ src/components/Category.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { Category } from './Category'
-import { categories } from '../data/fixtures';
+import { categories, clues } from '../data/fixtures';
+import { fakeServer } from 'sinon';
 
 const props = { category: categories[0] }
 
 describe('Category', () => {
+  beforeEach(() => {
+    const server = fakeServer.create();
+
+    server.respondWith(
+      'GET',
+      `http://jservice.io/api/clues?category=${props.category.id}`,
+      [
+        200,
+        { 'Content-Type': 'application/json' },
+        JSON.stringify(clue);
+      ]
+    )
+  })
   const category = mount(<Category { ...props }/>)
 })
