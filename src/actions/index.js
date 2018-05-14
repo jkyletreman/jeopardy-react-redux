a0b077d1f894a02c3e4b4e4f3cf1a2dd8d056e2a
Kyle Treman <jkt290@nyu.edu>
2018-05-13 21:45:17 -0400

    Add action for setting category

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+const SET_CATEGORIES = 'SET_CATEGORIES';
+
+const setCategories = (categories) => {
+  return {
+    type: SET_CATEGORIES,
+    categories
+  }
+}
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  async componentDidMount() {
+    const response = await fetch("http://jservice.io/api/categories?count=20");
+    const json = await response.json();
+  }
+  
   render() {
     return (
       <div>
