b62ab9b87fb2f18ecc037ad7dc031bb4bbe7ea1c
Kyle Treman <jkt290@nyu.edu>
2018-05-14 13:47:30 -0400

    export for testing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setCategories, pickCategory } from '../actions'
 
-class App extends Component {
+export class App extends Component {
   async componentDidMount() {
     if (this.props.categories.length === 0) {
       const response = await fetch("http://jservice.io/api/categories?count=20");
