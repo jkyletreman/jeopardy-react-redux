37ed824a53a6c5a6bea543a65857b154bcad90a5
Kyle Treman <jkt290@nyu.edu>
2018-05-14 11:23:52 -0400

    Connect fetch and redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setCategories } from '../actions'
 
 class App extends Component {
 
   async componentDidMount() {
-    const response = await fetch("http://jservice.io/api/categories?count=20");
-    const json = await response.json();
+    if (this.props.categories.length === 0) {
+      const response = await fetch("http://jservice.io/api/categories?count=20");
+      const json = await response.json();
+      this.props.setCategories(json);
+    }
   }
-  
+
   render() {
+    console.log(this.props)
+
     return (
       <div>
         <h2>Jeopardy!</h2>
@@ -15,5 +22,9 @@ class App extends Component {
     )
   }
 }
-
-export default App;
+// setting redux state
+const mapStateToProps = (state) => {
+  return { categories: state }
+}
+// connect(mapStateToProps, { what needs to be available as props })(Component)
+export default connect(mapStateToProps, { setCategories })(App);
