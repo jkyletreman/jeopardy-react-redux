1896baa6095464157dec596f1003c4131b951bb6
Kyle Treman <jkt290@nyu.edu>
2018-05-14 13:51:10 -0400

    Add mock data for tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { App } from './App';
+import { categories } from '../data/fixtures'
+
+const props = { categories };
 
 describe('App', () => {
-  const app = shallow(<App />)
+  const app = shallow(<App { ...props }/>)
 })
