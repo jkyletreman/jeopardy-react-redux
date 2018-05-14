7fdb4262d661e6f0d3a142ac0b205fde8539492b
Kyle Treman <jkt290@nyu.edu>
2018-05-14 13:06:12 -0400

    Fix render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { setCategories } from '../actions'
 
 class App extends Component {
-
   async componentDidMount() {
     if (this.props.categories.length === 0) {
       const response = await fetch("http://jservice.io/api/categories?count=20");
@@ -13,11 +13,18 @@ class App extends Component {
   }
 
   render() {
-    console.log(this.props)
-
     return (
       <div>
         <h2>Jeopardy!</h2>
+        {
+          this.props.categories.map(category => {
+            return (
+              <div key={category.id}>
+                <Link to='/category'><h4>{category.title}</h4></Link>
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
